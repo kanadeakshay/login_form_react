e050c20cd9e820f450a181e352bd7b5f8d9ae516
akshay782 <akshayk0782@gmail.com>
2022-03-29 00:57:10 +0530

    :bulb: update: comments & component

--- src/containers/home/home.js
+++ src/containers/home/home.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Input from '../../components/Input';
 
 const Home = () => {
 
@@ -12,15 +13,20 @@ const Home = () => {
         load: false
     });
 
+    // on submit function
     const login = (e) => {
+        // prevent default refresh of form
         e.preventDefault();
 
+        // if password and confirm password are wrong then display message
         if (password != confirmPassword) {
             setError("Password is not matching");
         } else {
+        // storing data in new object
             const loginData = { load: true, name, email, password, confirmPassword };
             setLoginCredential(loginData);
 
+        // resetting input fields as blank again after submitting of form
             setName("");
             setEmail("");
             setPassword("");
@@ -31,42 +37,28 @@ const Home = () => {
 
     return (
         <>
+        {/* conditional rendering depending upon did user filled the the form or not */}
             {
                 loginCredential.load === false ?
                     <form action='' onSubmit={login}>
-                        <div>
-                            <label htmlFor="name">Name</label>
-                            <input type="text" name="name" id="name" autoComplete='off'
-                                required
-                                value={name}
-                                onChange={(e) => setName(e.target.value)}
-                            />
-                        </div>
-                        <div>
-                            <label htmlFor="email">Email</label>
-                            <input type="email" name="email" id="email" autoComplete='off'
-                                required
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                            />
-                        </div>
-                        <div>
-                            <label htmlFor="password">Password</label>
-                            <input type="password" name="password" id="password" autoComplete='off'
-                                required
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                            />
-                        </div>
-                        <div>
-                            <label htmlFor="confirmPassword">Confirm Password</label>
-                            <input type="password" name="confirmPassword" id="confirmPassword" autoComplete='off'
-                                required
-                                value={confirmPassword}
-                                onChange={(e) => setConfirmPassword(e.target.value)}
-                            />
-                        </div>
+                        <Input htmlFor="name" type="text" name="name" id="name"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                        <Input htmlFor="email" type="email" name="email" id="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <Input htmlFor="password" type="password" name="password" id="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <Input htmlFor="confirmPassword" type="confirmPassword" name="confirmPassword" id="confirmPassword"
+                            value={confirmPassword}
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                        />
 
+                        {/* displaying error message if both password did not matched */}
                         {
                             error === "" ?
                                 "" : <h3>{error}</h3>
@@ -76,17 +68,14 @@ const Home = () => {
                     </form>
 
                     :
-
+                    
+                    // display the credentials that are entered 
                     <div>
                         <p>Your Name -{loginCredential.name}</p>
                         <p>Your email - {loginCredential.email}</p>
                     </div>
 
             }
-
-
-
-
         </>
     )
 }
